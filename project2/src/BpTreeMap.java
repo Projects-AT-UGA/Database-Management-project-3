62fc2e6f38c5cc190962a0fce397f8be1b19f218
prathyuk9 <prathyusha.kurapati26@gmail.com>
2024-02-18 23:05:06 -0500

    Added BPTreeMap entry set

--- project2/src/BpTreeMap.java
+++ project2/src/BpTreeMap.java
@@ -255,9 +255,26 @@ public class BpTreeMap <K extends Comparable <K>, V>
         var enSet = new HashSet <Map.Entry <K, V>> ();
 
         //  T O   B E   I M P L E M E N T E D
-
+        traverse_helper(root, enSet);
         return enSet;
     } // entrySet
+    private void traverse_helper(BpTreeMap<K, V>.Node node, Set<Map.Entry<K, V>> entrySet) {
+        if (node != null) {
+            // If node is a leaf, add its entries to the set
+            if (node.isLeaf) {
+                for (int i = 0; i < node.keys; i++) {
+                    K key = node.key[i];
+                    V value = (V) node.ref[i + 1];
+                    entrySet.add(new AbstractMap.SimpleEntry<>(key, value));
+                }
+            } else {
+                // If node is an internal node, recursively traverse its children
+                for (int i = 0; i <= node.keys; i++) {
+                    traverse_helper((BpTreeMap<K, V>.Node) node.ref[i], entrySet);
+                }
+            }
+        }
+    }
 
     /********************************************************************************
      * Given the key, look up the value in the B+Tree map.
